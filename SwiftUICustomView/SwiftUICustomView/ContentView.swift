bacc647bb8ef34b6c606d2b5f0a307c8887ceafd
kevinabrioux <kevin.abrioux@gmail.com>
2020-03-23 16:40:17 +0100

    first commit

---
+++ SwiftUICustomView/SwiftUICustomView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICustomView
+//
+//  Created by Kevin ABRIOUX on 23/03/2020.
+//  Copyright © 2020 Kevin ABRIOUX. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
